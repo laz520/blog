502a3f3faf631c7bbcb9ce4b70560e84f18a0804
16750145927 <2104819695@qq.com>
2022-04-26 19:08:57 +0800

    童年博客

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ include ("php/function.php");
                 while ($rw = $result->fetch_assoc()) {
 
 
-                    ?>              <?php    // $absract =strip_tags($rw['content']11);
+                    ?>              <?php    // $absract =strip_tags($rw['content']111);
                     $absract =mb_substr($rw['content'],0,59,"UTF-8")  ?>
                     <div class="p-4  " role="alert">
                         <div class="card">
